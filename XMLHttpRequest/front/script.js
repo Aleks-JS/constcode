172d6a3c032f4cfb580d321228da8773ba0270d8
Aleksey Kresnikov <kresnikov@yandex.ru>
2020-09-24 12:25:10 +0300

    Receiving data via XMLHttpRequest

--- XMLHttpRequest/front/script.js
+++ XMLHttpRequest/front/script.js
@@ -1,9 +1,20 @@
-$.ajax("http://localhost:3000/users/4", {
-  method: "PUT",
-  data: {
-    name: "Maxim",
-  },
-  success(data) {
-    document.querySelector("pre").textContent = JSON.stringify(data, null, 3);
-  },
-});
+// $.ajax("http://localhost:3000/users/4", {
+//   method: "PUT",
+//   data: {
+//     name: "Maxim",
+//     surname: "Green",
+//   },
+//   success(data) {
+//     document.querySelector("pre").textContent = JSON.stringify(data, null, 3);
+//   },
+// });
+
+const request = new XMLHttpRequest();
+
+request.onload = function () {
+  const data = JSON.parse(this.responseText);
+  console.log(data);
+};
+
+request.open("get", "http://localhost:3000/users/", true);
+request.send();
