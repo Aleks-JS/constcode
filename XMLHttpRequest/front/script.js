025902d52d330eacbf17230191984efa37b400e0
Aleksey Kresnikov <kresnikov@yandex.ru>
2020-09-24 12:13:22 +0300

    Changing an object field using the PATCH method

--- XMLHttpRequest/front/script.js
+++ XMLHttpRequest/front/script.js
@@ -1,5 +1,8 @@
-$.ajax("http://localhost:3000/users/5", {
-  method: "DELETE",
+$.ajax("http://localhost:3000/users/4", {
+  method: "PATCH",
+  data: {
+    name: "Maxim",
+  },
   success(data) {
     document.querySelector("pre").textContent = JSON.stringify(data, null, 3);
   },
