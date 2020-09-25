975febaa11aa7442f88194c033f50bcc00ee1d83
Aleksey Kresnikov <kresnikov@yandex.ru>
2020-09-25 07:27:16 +0300

    Editing data on the server using fetch

--- XMLHttpRequest/front/script.js
+++ XMLHttpRequest/front/script.js
@@ -10,6 +10,7 @@
 //   },
 // });
 
+/************************************************************** */
 // Работа с запросами через XMLHttpRequest. Устаревший метод
 // const request = new XMLHttpRequest();
 
@@ -21,20 +22,82 @@
 // request.open("get", "http://localhost:3000/users/", true);
 // request.send();
 
+/************************************************************* */
 // Работа с запросами через созданный метод, наподобие JQuery
-const ajax = (url, param) => {
-  const request = new XMLHttpRequest();
+// const ajax = (url, param) => {
+//   const request = new XMLHttpRequest();
 
-  request.onload = function () {
-    param.success(JSON.parse(this.responseText));
-  };
-  request.open(param.method ?? "GET", url, true);
-  request.send();
-};
+//   request.onload = function () {
+//     param.success(JSON.parse(this.responseText));
+//   };
+//   request.open(param.method ?? "GET", url, true);
+//   request.send();
+// };
 
-ajax("http://localhost:3000/users", {
-  method: "GET",
-  success(data) {
-    console.log(data);
+// ajax("http://localhost:3000/users", {
+//   method: "GET",
+//   success(data) {
+//     console.log(data);
+//   },
+// });
+
+/************************************************************* */
+// Работа с запросами через Fetch
+// fetch("http://localhost:3000/users")
+//   .then((result) => result.json())
+//   .then((data) => console.log(data));
+
+// // Работа с запросами через Fetch, используя async-await
+// main();
+
+// async function main() {
+//   const response = await fetch("http://localhost:3000/users");
+//   const data = await response.json();
+//   console.log(data);
+// }
+
+// Отправка данных на сервер
+// fetch("http://localhost:3000/users/", {
+//   method: "POST",
+//   headers: {
+//     "content-type": "application/json",
+//   },
+//   body: JSON.stringify({ name: "Natalia", surname: "Uvarova" }),
+// })
+//   .then((result) => result.json())
+//   .then((data) => console.log(data));
+
+// Частичное редактирование данных объекта на сервере
+// fetch("http://localhost:3000/users/4", {
+//   method: "PATCH",
+//   headers: {
+//     "content-type": "application/json",
+//   },
+//   body: JSON.stringify({
+//     name: "Stepan",
+//     surname: "Kresnikov",
+//   }),
+// })
+//   .then((r) => r.json())
+//   .then(
+//     (d) =>
+//       (document.querySelector("pre").textContent = JSON.stringify(d, null, 3))
+//   );
+
+// Частичное редактирование данных объекта на сервере
+fetch("http://localhost:3000/users/5", {
+  method: "PUT",
+  headers: {
+    "content-type": "application/json",
   },
-});
+  body: JSON.stringify({
+    id: 5,
+    name: "Alex",
+    surname: "Kresnikov",
+  }),
+})
+  .then((r) => r.json())
+  .then(
+    (d) =>
+      (document.querySelector("pre").textContent = JSON.stringify(d, null, 3))
+  );
